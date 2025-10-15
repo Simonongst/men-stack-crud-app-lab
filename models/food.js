77350f8e0786caaf2241f3cc3d73db748590e968
Simon Ong <simonongst@gmail.com>
2025-10-15 23:31:54 +0800

    feat(models): define Food schema with name, description, and image fields

---
+++ models/food.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const foodSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true},
+    image: String,
+});
+
+const Food = mongoose.model('Food', foodSchema);
+
+module.exports = Food;
